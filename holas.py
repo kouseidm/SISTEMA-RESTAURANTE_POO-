303ee70a215001a96e1ca5b612dac20abe45643c
KouseiDM <huamanfloresalexis09@gmail.com>
2025-05-01 01:58:35 -0500

    2

--- holas.py
+++ holas.py
@@ -1,1 +1,14 @@
 print ("Hola perras")
+
+
+
+
+print("Horas de codigo abierto para principantes de la comunidad de codigo abierto")
+print("Hola mundo")
+print("Hola mundo 2")
+
+
+print("Hola mundo 4")
+print("Hola mundo 5")
+print("Hola mundo 6")
+print("Hola mundo 7")
